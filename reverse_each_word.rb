28848c7edb7a65197f18eb04619ce3bbaac96069
Hannah Angle <hannahcangle@gmail.com>
2020-08-01 05:42:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,6 @@
-require "pry"
 def reverse_each_word(string)
   array = string.split(" ")
-  # binding.pry
   array.collect do |word|
   new_array << word.reverse
-    # binding.pry
-  end.join
-  # binding.pry
-  
+ end.join
 end
